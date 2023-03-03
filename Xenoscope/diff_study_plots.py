8777911b1594b8c620645d0993caec6908c29e02
Ricardo Peres <rperes@physik.uzh.ch>
2023-03-03 01:39:35 +0100

    More diffusion plots

--- Xenoscope/diff_study_plots.py
+++ Xenoscope/diff_study_plots.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import xenodiffusionscope
 
 def nest_diff_trans_model(field): #cm2/s
     ans = (37.368 * np.power(field, .093452) * 
@@ -43,8 +44,54 @@ def plot_transv_diff_literature():
     fig.savefig('Figures/D_transverse_literature.pdf')
     
 
+
+def plot_lamp_pulse():
+    lamp = xenodiffusionscope.XeLamp(1)
+    fig, ax = plt.subplots(1,1,figsize = (3.5,3))
+    _t = np.linspace(0,6,100)
+    _pulse = lamp.pulse_lamp(_t)
+    ax.plot(_t, _pulse/max(_pulse))
+    ax.set_ylabel('Relative intensity')
+    ax.set_xlabel('t [$\mu$s]')
+    fig.savefig('Figures/lamp_pulse.pdf')
+
+def plot_init_pos():
+    lamp = xenodiffusionscope.XeLamp(1)
+    population = lamp.emitted_electrons_in_interval(0,6)
+    x0,y0,z0 = lamp.init_positions(population)
+    fig, ax = plt.subplots(1,1,figsize=(3.5,3))
+    hh = ax.hist2d(x0,y0, bins = 200, cmin = 1, cmap = coolwarm)
+    ax.set_aspect('equal')
+    ax.set_ylim(-3,3)
+    ax.set_xlim(-3,3)
+    ax.set_xlabel('x [mm]')
+    ax.set_ylabel('y [mm]')
+    fig.colorbar(hh[3],label= 'Number of initial electrons')
+    fig.savefig('Figures/init_positions.png')
+
+
 if __name__ == '__main__':
     # Load my style ;)
 
     plt.style.use('../thesis_style.mplstyle')
-    plot_transv_diff_literature()
+
+    #the lovely coolwarm colormap :)
+
+    from matplotlib.colors import ListedColormap
+
+    cm = plt.cm.get_cmap('coolwarm')
+
+    # Get the colormap colors, multiply them with the factor "a", and create new colormap
+    a = 0.85
+    coolwarm = plt.cm.coolwarm(np.arange(plt.cm.coolwarm.N))
+    coolwarm[:,0:3] *= a 
+    coolwarm = ListedColormap(coolwarm)
+
+    summer = plt.cm.summer(np.arange(plt.cm.summer.N))
+    summer[:,0:3] *= a 
+    summer = ListedColormap(summer)
+    
+    ### Plots
+    #plot_transv_diff_literature()
+    plot_lamp_pulse()
+    plot_init_pos()
