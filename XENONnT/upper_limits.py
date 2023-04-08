541e287c47757e3be97a4ca8e2a12122777250a9
Ricardo Peres <rperes@physik.uzh.ch>
2023-04-09 00:51:50 +0200

    Update on SI limits plot

--- XENONnT/upper_limits.py
+++ XENONnT/upper_limits.py
@@ -8,7 +8,7 @@ def load_files_SI():
     cdms = np.loadtxt("Data/limits/CDMSLite2018_SI.csv",delimiter=",")
     cresst = np.loadtxt("Data/limits/CRESSTII_SI_2016_v2.csv",delimiter=",")
     damic = np.loadtxt("Data/limits/DAMIC_2020.csv",delimiter=",")
-    darkside = np.loadtxt("Data/limits/darkside50_ul.csv",delimiter=",")
+    darkside_low = np.loadtxt("Data/limits/darkside50_lowmass_ul.csv",delimiter=",")
     darwin = np.loadtxt("Data/limits/darwin_SI.csv",delimiter=",")
     deap = np.loadtxt("Data/limits/Deap_SI_2018.csv",delimiter=",")
     lux = np.loadtxt("Data/limits/lux_SI.csv",delimiter=",")
@@ -18,45 +18,85 @@ def load_files_SI():
     xenonnt = np.loadtxt("Data/limits/xenonnt_SI.csv",delimiter=",")
     xenon1t_2fold = np.loadtxt("Data/limits/xenon1t_2fold_SI.csv",delimiter=",")
     xenon1t_s2only = np.loadtxt("Data/limits/xenon1t_s2only_SI.csv",delimiter=",")
-    
-    limits_LXe = {'XENONnT' : xenonnt,
-                  'LZ' : lz,
-                  'PandaX-4T' : pandax,
-                  'XENON1T (2-fold)' : xenon1t_2fold,
-                  'XENON1T (S2-only)' : xenon1t_s2only,
-                  'DARWIN (projection)' : darwin}
-    limits_Ar = {'DEAP' : deap,
-                 'Darkside50' : darkside}
-    limits_other = {'DAMIC' : damic,
-                    'CDMS' : cdms,
-                    'CRESST' : cresst}
-    limits_neutrino = {'Neutrino fog' : neutrino}
+    supercdms = np.loadtxt("Data/limits/SuperCDMS_SI.csv",delimiter=",")
+    darkside_high = np.loadtxt("Data/limits/DS-50.csv",delimiter=",")
 
-    return limits_LXe,limits_Ar,limits_other,limits_neutrino
+    limits = {'XENONnT' : xenonnt,
+              'LZ' : lz,
+              'PandaX-4T' : pandax,
+              'XENON1T (2-fold)' : xenon1t_2fold,
+              'XENON1T (S2-only)' : xenon1t_s2only,
+              'DARWIN (projection)' : darwin,
+              'DEAP-3600' : deap,
+              'Darkside-50_low' : darkside_low,
+              'Darkside-50_high' : darkside_high,
+              'DAMIC' : damic,
+              'CDMSLite' : cdms,
+              'SuperCDMS' : supercdms,
+              'CRESST' : cresst,
+              'Neutrino fog' : neutrino}
+
+    return limits
 
 
 def make_plot_SI(limits):
     print('Making SI limits plot.')
-    limits_LXe,limits_Ar,limits_other,limits_neutrino = limits
 
-    fig, ax = plt.subplots(1,1,figsize = (6,3))
+    fig, ax = plt.subplots(1,1,figsize = (6,2.65))
 
-    for limit_key in limits_LXe.keys():
-        ax.plot(limits_LXe[limit_key][:,0],limits_LXe[limit_key][:,1], 
-                ls = '-', label = limit_key)
-    for limit_key in limits_Ar.keys():
-        ax.plot(limits_Ar[limit_key][:,0],limits_Ar[limit_key][:,1],
-                ls = '--', label = limit_key)
-    for limit_key in limits_other.keys():
-        ax.plot(limits_other[limit_key][:,0],limits_other[limit_key][:,1],
-                ls = '-.', label = limit_key)
-    for limit_key in limits_neutrino.keys():
-        ax.fill_between(limits_neutrino[limit_key][:,0],
-                        0,
-                        limits_neutrino[limit_key][:,1],
-                        ls = ':', label = limit_key, 
-                        alpha = 0.3,
-                        color = 'C6')
+    #Xe
+    limit_key = 'XENONnT'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+            ls = '-', label = limit_key, color = 'C0',
+            zorder = 10)
+    limit_key = 'LZ'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+                ls = '-', label = limit_key, color = 'C1')
+    limit_key = 'PandaX-4T'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+                ls = '-', label = limit_key, color = 'C2')
+    limit_key = 'XENON1T (2-fold)'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+                ls = '-', label = limit_key, color = 'C3')
+    limit_key = 'XENON1T (S2-only)'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+                ls = '-', label = limit_key, color = 'C4')
+    limit_key = 'DARWIN (projection)'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+                ls = '-', label = limit_key, color = 'C5')
+    
+    #Ar
+    limit_key = 'DEAP-3600'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+            ls = '--', label = limit_key, color = 'C6')
+    limit_key = 'Darkside-50_low'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+            ls = '--', label = 'Darkside-50', color = 'C7')
+    limit_key = 'Darkside-50_high'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1], 
+            ls = '--', color = 'C7')
+
+    #Other
+    limit_key = 'DAMIC'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1],
+            ls = '-.', label = limit_key, color = 'C8')
+    limit_key = 'SuperCDMS'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1],
+            ls = '-.', label = limit_key, color = 'C9')
+    limit_key = 'CDMSLite'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1],
+            ls = '-.', color = 'C9')
+    limit_key = 'CRESST'
+    ax.plot(limits[limit_key][:,0],limits[limit_key][:,1],
+            ls = '-.', label = limit_key, color = 'C10')
+
+    limit_key = 'Neutrino fog'
+    ax.fill_between(limits[limit_key][:,0],
+                    0,
+                    limits[limit_key][:,1],
+                    ls = ':', label = limit_key, 
+                    alpha = 0.7,
+                    color = 'lightgrey')
 
     ax.set_xscale('log')
     ax.set_yscale('log')
