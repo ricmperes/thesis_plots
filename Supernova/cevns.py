c6e785eba7249f5ff774815fdf50b0e2cf7db44c
Ricardo Peres <rperes@physik.uzh.ch>
2023-04-12 02:00:39 +0200

    More isotopes in form factor

--- Supernova/cevns.py
+++ Supernova/cevns.py
@@ -4,21 +4,33 @@ import astropy.units as u
 from multimessenger.supernova.Nucleus import Target
 from multimessenger.supernova.Xenon_Atom import ATOM_TABLE
 from matplotlib.colors import ListedColormap
+from matplotlib.colors import ListedColormap, LogNorm
 
+
+# Get the colormap colors, multiply them with the factor "a", and create new colormap
+a = 0.85
+coolwarm = plt.cm.coolwarm(np.arange(plt.cm.coolwarm.N))
+coolwarm[:,0:3] *= a 
+coolwarm = ListedColormap(coolwarm)
 
 def plot_formfactor():
-    recoil_en = np.linspace(0,200,100) * u.keV
-    singleXe = Target(ATOM_TABLE['Xe131'], pure=True)
-    formfac = singleXe.form_factor(recoil_en)
-
     fig, ax = plt.subplots(1,1,figsize = (4.5,3))
-    ax.plot(recoil_en, formfac**2)
+    
+    xe_isotopes = ['Xe129','Xe131','Xe132','Xe134','Xe136',]
+    for xe in xe_isotopes:
+        recoil_en = np.linspace(0,200,100) * u.keV
+        singleXe = Target(ATOM_TABLE[xe], pure=True)
+        formfac = singleXe.form_factor(recoil_en)
+
+        
+        ax.plot(recoil_en, formfac**2, label = ATOM_TABLE[xe]['Type'])
     ax.set_yscale('log')
     ax.set_xlabel('E$_R$ [keV]')
     ax.set_ylabel('$F(q^2)^2$')
     ax.set_xlim(0,200)
-    ax.set_ylim(3e-7, 1)
-    fig.savefig('Figures/form_factor.pdf')
+    ax.set_ylim(1e-7, 1)
+    ax.legend()
+    fig.savefig('Figures/form_factor_all_isotopes.pdf')
 
 def plot_cevns_matrix():
     recoil_en = np.linspace(0,50,100) * u.keV
@@ -29,7 +41,7 @@ def plot_cevns_matrix():
     fig, ax = plt.subplots(1,1,figsize = (4.5,3))
     matrix = ax.pcolormesh(neutrino_en.value, recoil_en.value, 
                            crosssec.to(u.cm**2/u.keV), norm = 'linear', 
-                    vmin = 0, cmap='coolwarm') #, norm=LogNorm())
+                    vmin = 0, cmap=coolwarm) #, norm=LogNorm())
     fig.colorbar(matrix, label = "$d\sigma/(dE_\\nu \cdot dE_R)$ [cm$^2$/keV/MeV]")
     ax.set_ylabel('$E_R [keV]$')
     ax.set_xlabel('$E_\\nu [MeV]$')
